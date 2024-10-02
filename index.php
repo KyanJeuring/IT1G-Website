677e4146937a26255c365caf919cc40e1f7036fe
Peter Kapsiar <kapsiar888n@gmail.com>
2024-10-02 10:35:46 +0200

    linked main.css to index.php

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sunny socks</title>
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+    
 </head>
 <body>
-    
+    <div id="header"></div>
+    <div id="content"></div>
+    <div id="footer"></div>
 </body>
 </html>
