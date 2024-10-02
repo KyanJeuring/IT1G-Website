86556030a5b4b5007b3974b1b1e2a0572083f881
KyanJeuring <kyanjeuring@gmail.com>
2024-10-02 16:44:03 +0200

    Added favicons and corrected the html setup in landingPage.php

--- content/landingPage.php
+++ content/landingPage.php
@@ -1,4 +1,16 @@
-<div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+    <link rel="icon" href="../resources\Favicons\ico\SunnySocksIcon.ico" type="image/x-icon">
+</head>
+<body>
+    <div>
     <h1>main page content</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias provident eos doloribus quas aspernatur. Suscipit nihil dignissimos aliquid animi neque voluptatem, fugit quam voluptas ad hic natus. Eaque, deserunt quia.</p>
 </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sunny socks</title>
     <link rel="stylesheet" href="css/main.css" type="text/css">
-
+    <link rel="icon" href="../resources\Favicons\ico\SunnySocksIcon.ico" type="image/x-icon">
     <script src="javascript/main.js" type="module"></script>
 </head>
 <body>
