f4e619d28eca51d31cf14a6200449398c601c1da
Victoria Iascevschi <victoria.iascevschi@student.nhlstenden.com>
2024-10-04 12:24:40 +0200

    added test paragraph

--- content/landingPage.php
+++ content/landingPage.php
@@ -11,6 +11,7 @@
     <div>
     <h1>main page content</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias provident eos doloribus quas aspernatur. Suscipit nihil dignissimos aliquid animi neque voluptatem, fugit quam voluptas ad hic natus. Eaque, deserunt quia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt harum quae optio distinctio? Possimus ullam vitae, iste doloremque officiis in? In sapiente debitis tempora doloribus iusto corporis commodi aspernatur.</p>
 </div>
 </body>
 </html>
