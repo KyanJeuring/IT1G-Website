4ef253fa224c4bfa58aacf99d28104c3a178e751
KyanJeuring <kyanjeuring@gmail.com>
2024-10-06 07:35:48 +0200

    Fixed indentation in aboutPage.php

--- content/aboutPage.php
+++ content/aboutPage.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us</title>
-    <link rel="stylesheet" href="../css/main.css" type="text/css">
-    <link rel="stylesheet" href="../css/navbarFooter.css" type="text/css">
-    <link rel="icon" href="../resources/Favicons/ico/SunnySocksIconBlue.ico" type="image/x-icon">
-</head>
-<body>
-    <div>
-    <h1>main page content</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias provident eos doloribus quas aspernatur. Suscipit nihil dignissimos aliquid animi neque voluptatem, fugit quam voluptas ad hic natus. Eaque, deserunt quia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt harum quae optio distinctio? Possimus ullam vitae, iste doloremque officiis in? In sapiente debitis tempora doloribus iusto corporis commodi aspernatur.</p>
-</div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>About Us</title>
+        <link rel="stylesheet" href="../css/main.css" type="text/css">
+        <link rel="stylesheet" href="../css/navbarFooter.css" type="text/css">
+        <link rel="icon" href="../resources/Favicons/ico/SunnySocksIconBlue.ico" type="image/x-icon">
+    </head>
+    <body>
+        <div>
+            <h1>main page content</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias provident eos doloribus quas aspernatur. Suscipit nihil dignissimos aliquid animi neque voluptatem, fugit quam voluptas ad hic natus. Eaque, deserunt quia.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt harum quae optio distinctio? Possimus ullam vitae, iste doloremque officiis in? In sapiente debitis tempora doloribus iusto corporis commodi aspernatur.</p>
+        </div>
+    </body>
 </html>
