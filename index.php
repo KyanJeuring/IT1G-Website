067918726ff19998050223421d99fa85a9c1e8af
Peter Kapsiar <kapsiar888n@gmail.com>
2024-10-06 18:57:25 +0200

    added basic header

--- index.php
+++ index.php
@@ -4,13 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sunny socks</title>
+    
+    <link rel="stylesheet" href="css/header.css" type="text/css">
     <link rel="stylesheet" href="css/main.css" type="text/css">
     <link rel="icon" href="./resources\Favicons\ico\SunnySocksIcon.ico" type="image/x-icon">
     <script src="javascript/main.js" type="module"></script>
 </head>
 <body>
-    <div id="header"></div>
+    <div id="header">
+        <div class="section left">
+            <button id="logoButton" href="index.php">
+                <img src="resources/Logo's/png/sunny_logos_blue.png" alt="Sunny socks logo">
+            </button>
+        </div>
+        <div class="section center">    
+            <button type="button">ABOUT</button>
+            <button type="button">MEN</button>
+            <button type="button">WOMEN</button>
+            <button type="button">SHOP</button>
+            <button type="button">CONTACT</button>
+        </div>
+        <div class="section right">
+            <button><img src="resources/icons/svg/searchIcon.svg" alt="searchIcon"></button>
+            <button><img src="resources/icons/svg/shoppingBagIcon.svg" alt="cartIcon"></button>
+            <button><img src="resources/icons/svg/userIcon.svg" alt="profileIcon"></button>
+        </div>
+    </div>
     <div id="content"></div>
     <div id="footer"></div>
 </body>
-</html> 
+</html>
