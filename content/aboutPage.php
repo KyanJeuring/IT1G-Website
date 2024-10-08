37ca37488ae17fccbbe9f331636b58e728741712
KyanJeuring <kyanjeuring@gmail.com>
2024-10-08 18:31:13 +0200

    Removed title tages in the PHP files in the content folder

--- content/aboutPage.php
+++ content/aboutPage.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>About</title>
     </head>
     <body>
         <h1>About page content</h1>
