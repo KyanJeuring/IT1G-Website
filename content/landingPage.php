a9e119e83ec32eb9e89a321c85632f3f2a29989b
Alexandros Karayiannis <alex-kar@hotmail.com>
2024-10-10 13:10:28 +0200

    added demo text to landing page

--- content/landingPage.php
+++ content/landingPage.php
@@ -8,5 +8,7 @@
         <h1>Main page content</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias provident eos doloribus quas aspernatur. Suscipit nihil dignissimos aliquid animi neque voluptatem, fugit quam voluptas ad hic natus. Eaque, deserunt quia.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt harum quae optio distinctio? Possimus ullam vitae, iste doloremque officiis in? In sapiente debitis tempora doloribus iusto corporis commodi aspernatur.</p>
+
+        <p>demo text demo text demo text demo text demo text demo text demo text </p>
     </body>
 </html>
