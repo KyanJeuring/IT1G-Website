cc005a2995a6a3e87232ce0f91fa276ff18ae7fa
Victoria Iascevschi <victoria.iascevschi@student.nhlstenden.com>
2024-10-13 22:58:03 +0200

    CompanyValues added one section on About page

--- content/aboutPage.php
+++ content/aboutPage.php
@@ -1,1 +1,35 @@
-<div class="OurValues"><img src="resources/otherPhotos/NaturePicture.png" alt="Flowers"></div>
+<section class="backgroundImageContainer">
+    <h1>OUR VALUES</h1>
+    <div class="ContentOne">
+        <div class="leftSideBox">
+            <div class="headingOne"><h2>POSITIVITY</h2></div>
+            <div class="partOne">
+                Positivity is ingrained in our DNA. 
+                Sunny is all about embracing the bright side of life. We see every challenge as
+                an opportunity to innovate and deliver solutions that not only meet needs 
+                but exceed expectations.
+            </div>
+        </div>
+        <div class="rightSideBox">
+            <div class="headingTwo"><h3>COLORFUL</h3></div>
+            <div class="partTwo">We bring vibrancy to the everyday with socks that add a pop of color to your wardrobe. 
+                By transforming the mundane—basic white socks—into a canvas of creativity,
+                 we celebrate individuality and joy.
+            </div>
+        </div>
+        <div class="leftSideBoxTwo">       
+            <div class="headingThree"><h4>BENEFICIAL</h4></div>
+            <div class="partThree">
+                At Sunny, we strive for win-win scenarios. Our business model ensures that everyone involved—from the materials 
+                suppliers to our factory workers—benefits. Our products are crafted with respect for the Earth, 
+                offering fair wages in our factories, fair profits for our retailers, and high-quality, 
+                reasonably priced socks for our customers.
+            </div>
+        </div>
+    
+    </div>
+    <div class="filler">
+        
+    </div>
+   
+</section>
