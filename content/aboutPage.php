afd3d0979a4fdbe0e9d01eb0d94a63f7cbea573e
Victoria Iascevschi <victoria.iascevschi@student.nhlstenden.com>
2024-10-14 10:43:08 +0200

    I added the family picture and the nature picture to otherPhotos and finished with te section Our values

--- content/aboutPage.php
+++ content/aboutPage.php
@@ -12,24 +12,22 @@
         </div>
         <div class="rightSideBox">
             <div class="headingTwo"><h3>COLORFUL</h3></div>
-            <div class="partTwo">We bring vibrancy to the everyday with socks that add a pop of color to your wardrobe. 
-                By transforming the mundane—basic white socks—into a canvas of creativity,
+                 <div class="partTwo">We bring vibrancy to the everyday with socks that add a pop of color to your wardrobe. 
+                 By transforming the mundane—basic white socks—into a canvas of creativity,
                  we celebrate individuality and joy.
-            </div>
+                 </div>
         </div>
         <div class="leftSideBoxTwo">       
             <div class="headingThree"><h4>BENEFICIAL</h4></div>
-            <div class="partThree">
-                At Sunny, we strive for win-win scenarios. Our business model ensures that everyone involved—from the materials 
-                suppliers to our factory workers—benefits. Our products are crafted with respect for the Earth, 
-                offering fair wages in our factories, fair profits for our retailers, and high-quality, 
-                reasonably priced socks for our customers.
+                <div class="partThree">
+                    At Sunny, we strive for win-win scenarios. Our business model ensures that everyone involved—from the materials 
+                    suppliers to our factory workers—benefits. Our products are crafted with respect for the Earth, 
+                    offering fair wages in our factories, fair profits for our retailers, and high-quality, 
+                    reasonably priced socks for our customers.
+                </div>
             </div>
         </div>
-    
-    </div>
-    <div class="filler">
-        
-    </div>
+     <div class="filler"> 
+     </div>
    
 </section>
