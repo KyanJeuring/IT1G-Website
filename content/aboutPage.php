5f859933d20b2d770c41f7658ad707f77a38413f
Victoria Iascevschi <victoria.iascevschi@student.nhlstenden.com>
2024-10-14 20:07:46 +0200

    I added the branch hero-header-about and finished this section. Also, I improved the section Our Values.

--- content/aboutPage.php
+++ content/aboutPage.php
@@ -1,25 +1,46 @@
+<section class=heroHeaderAboutContainer>
+    <div class="familyPicture">
+        <img src="resources/otherPhotos/familyPicture.png" alt="familyPicture">
+    </div>
+    <div class="companyMotives">
+        <h1>WE CARE ABOUT YOU</h1>
+        <h2>LET'S SAVE THE EARTH TOGETHER</h2>
+        <p>
+         At Sunny, we believe that even the simplest daily actions, 
+         like putting on your socks, hold the potential to uplift spirits 
+         and add a splash of joy to life.
+        </p>
+        <p>
+         Our name, Sunny, reflects our core ethos: to always look on the bright side.
+         Our products do more than just clothe your feet—they embody positivity, 
+         add color to your routine, and create benefits for everyone involved, 
+         from the ground up.
+        </p>
+    </div>
+</section>
 <section class="backgroundImageContainer">
     <h1>OUR VALUES</h1>
-    <div class="ContentOne">
-        <div class="leftSideBox">
-            <div class="headingOne"><h2>POSITIVITY</h2></div>
-            <div class="partOne">
+    <div class="contentOfThePage">
+        <div class="boxPositivity">
+            <div class="headingPositivity"><h2>POSITIVITY</h2></div>
+            <div class="positivityDescription">
                 Positivity is ingrained in our DNA. 
                 Sunny is all about embracing the bright side of life. We see every challenge as
                 an opportunity to innovate and deliver solutions that not only meet needs 
                 but exceed expectations.
             </div>
         </div>
-        <div class="rightSideBox">
-            <div class="headingTwo"><h3>COLORFUL</h3></div>
-                 <div class="partTwo">We bring vibrancy to the everyday with socks that add a pop of color to your wardrobe. 
+        <div class="boxColorful">
+            <div class="headingColorful"><h3>COLORFUL</h3></div>
+                 <div class="colorfulDescription">
+                 We bring vibrancy to the everyday with socks that add a pop of color to your wardrobe. 
                  By transforming the mundane—basic white socks—into a canvas of creativity,
                  we celebrate individuality and joy.
                  </div>
         </div>
-        <div class="leftSideBoxTwo">       
-            <div class="headingThree"><h4>BENEFICIAL</h4></div>
-                <div class="partThree">
+        <div class="boxBeneficial">       
+            <div class="headingBeneficial"><h4>BENEFICIAL</h4></div>
+                <div class="beneficialDescription">
                     At Sunny, we strive for win-win scenarios. Our business model ensures that everyone involved—from the materials 
                     suppliers to our factory workers—benefits. Our products are crafted with respect for the Earth, 
                     offering fair wages in our factories, fair profits for our retailers, and high-quality, 
