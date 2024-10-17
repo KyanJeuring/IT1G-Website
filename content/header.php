379f124b53cd9b9dde07cf8eaceb79b30688f4eb
Peter Kapsiar <kapsiar888n@gmail.com>
2024-10-17 17:37:13 +0200

    added arrow icons to header

--- content/header.php
+++ content/header.php
@@ -4,11 +4,26 @@
     </button>
 </form>
 <form class="section center" action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST">
-    <button id="aboutButton" type="submit" name="navBtn" value="About">ABOUT</button>
-    <button id="menButton" type="submit" name="navBtn" value="Shop">MEN</button>
-    <button id="womenButton" type="submit" name="navBtn" value="Shop">WOMEN</button>
-    <button id="shopButton" type="submit" name="navBtn" value="Shop">SHOP</button>
-    <button id="contactButton" type="submit" name="navBtn" value="Contact">CONTACT</button>
+    <button id="aboutButton" type="submit" name="navBtn" value="About" >
+        <p>ABOUT</p>
+        <img src="resources/icons/svg/downArrow.svg" alt="arrowIcon">
+    </button>
+    <button id="menButton" type="submit" name="navBtn" value="Shop">
+        <p>MEN</p>
+        <img src="resources/icons/svg/downArrow.svg" alt="arrowIcon">
+    </button>
+    <button id="womenButton" type="submit" name="navBtn" value="Shop">
+        <p>WOMEN</p>
+        <img src="resources/icons/svg/downArrow.svg" alt="arrowIcon">
+    </button>
+    <button id="shopButton" type="submit" name="navBtn" value="Shop">
+        <p>SHOP</p>
+        <img src="resources/icons/svg/downArrow.svg" alt="arrowIcon">
+    </button>
+    <button id="contactButton" type="submit" name="navBtn" value="Contact">
+        <p>CONTACT</p>
+        <img src="resources/icons/svg/downArrow.svg" alt="arrowIcon">
+    </button>
 </form>
 <form class="section right" form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST">
     <button><img class="icon" src="resources/icons/svg/searchIcon.svg" alt="searchIcon"></button>
