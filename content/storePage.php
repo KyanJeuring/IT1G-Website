085f69c28e81751b6ec6128a0a9f1d7ada604a1f
aradkashef <arad.kashef.haghighi@student.nhlstenden.com>
2024-10-18 11:27:28 +0200

    merged from main

--- content/storePage.php
+++ content/storePage.php
@@ -1,54 +1,121 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Store</title>
-    </head>
-    <body>
+<?php
+    if(!session_id()) session_start();
+?>
         <h1>Store page content</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias provident eos doloribus quas aspernatur. Suscipit nihil dignissimos aliquid animi neque voluptatem, fugit quam voluptas ad hic natus. Eaque, deserunt quia.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt harum quae optio distinctio? Possimus ullam vitae, iste doloremque officiis in? In sapiente debitis tempora doloribus iusto corporis commodi aspernatur.</p>
-        <div class="search-container">
-            <div class="filter-section">
-                <form action="" method="POST">
-                    <input type="search" name="search" placeholder="search">
-                    <button type="submit">Search</button>
-                </form>
-            </div>
-            <div class="product-list">
-                <?php
-                //list of products
-                $products = 
-                [
-                    ['name' => 'Ocean Breeze', 'color' => 'blue', 'design' => 'striped'],
-                    ['name' => 'Navy Drift', 'color' => 'light blue', 'design' => 'striped'],
-                    ['name' => 'Sunny Socks', 'color' => 'yellow', 'design' => 'solid'],
-                    ['name' => 'Glam Walkers', 'color' => 'pink', 'design' => 'striped'],
-                    ['name' => 'Crimson Web', 'color' => 'red', 'design' => 'striped'],
-                    ['name' => 'Cool Blue', 'color' => 'blue', 'design' => 'solid'],
-                    ['name' => 'Navy Drift', 'color' => 'light blue', 'design' => 'solid'],
-                    ['name' => 'Cotton Candy', 'color' => 'pink', 'design' => 'solid'],
-                    ['name' => 'Tomato', 'color' => 'red', 'design' => 'solid'],
-                    ['name' => 'Lemon Whirl', 'color' => 'yellow', 'design' => 'striped'],
-                    ['name' => 'Sporty', 'color' => 'white', 'design' => 'striped'],
-                    ['name' => 'Long Stripes', 'color' => 'multicolor','design' => 'striped'],
-                    ['name' => 'White Dream', 'color' => 'white', 'design' => 'solid'],
-                    ['name' => 'Grey Dream', 'color' => 'grey', 'design' => 'solid'],
-                    ['name' => 'Confusion', 'color' => 'multicolor', 'design' => 'unique'],
-                    ['name' => 'Wooble Double', 'color' => 'multicolor', 'design' => 'unique'],
-                    ['name' => 'Splashy Colors', 'color' => 'multicolor', 'design' => 'unique'],
-                    ['name' => 'Warm Flowers', 'color' => 'multicolor', 'design' => 'unique'],
-                ];
+<div id="container">
+    <aside>
+        <div class="categories">
+            <p>Product categories</p>
+            <p class="keywords">Keywords</p>
 
+            <div id="keywords">
+                <div class="spring">Spring x</div>
+                <div class="smart">Smart x</div>
+                <div class="modern">Modern x</div>
+            </div>
+
+            <form action="<?php echo $_SERVER['PHP_SELF'] ?>" class="form">
+                <div id="filter">
+                    <input type="checkbox" name="filter" value="men" checked="checked"> Men <br>
+                    <input type="checkbox" name="filter" value="women" checked="checked"> Women <br>
+                    <input type="checkbox" name="filter" value="kids" checked="checked"> Kids <br>
+                </div>
+
+                <div id="price">
+                    <label for="price">Price Range</label>
+                    <label for="price">$0-100</label>
+                    <input type="range" name="price" min="0" max="100" step="1" defauk="0" />
+                </div>
+
+                <div id="color">
+                    <p>Filter by Color</p>
+                    <input type="checkbox" name="color" value="red" checked="checked"> Red</input> <br>
+                    <input type="checkbox" name="color" value="blue" checked="checked"> Blue</input> <br>
+                    <input type="checkbox" name="color" value="yellow" checked="checked"> Yellow</input> <br>
+                </div>
+
+                <div id="size">
+                    <p>Filter by Size</p>
+                    <input type="checkbox" name="size" value="small" checked="checked"> Small</input> <br>
+                    <input type="checkbox" name="size" value="medium" checked="checked"> Medium</input> <br>
+                    <input type="checkbox" name="size" value="large" checked="checked"> Large</input> <br>
+                </div>
+            </form> 
+        </div>
+    </aside> 
+
+    <main>
+        <div id="bar">
+            <form action="" class="search">
+                <input type="search" name="search">
+                <label for="search">Search</label>
+            </form>
+        </div>
+
+        
+        <!-- <div id="products">
+            <div class="ocean-breeze">
+                <a href="">
+                    <img src="resources/sunny_socks_photos/catalogus/Sunny_socks_blue.jpg" alt="Ocean Breeze">
+                    <p>Ocean Breeze</p>
+                    <p>$25.99</p>
+                </a>
+            </div>
+        </div> -->
+        <div class="product-list">
+                <?php
+
+                    // product class
+                    class product
+                    {
+                        public $name;
+                        public $color;
+                        public $design;
+    
+                        public function __construct($name, $color, $design)
+                        {
+                            $this->name = $name;
+                            $this->color = $color;
+                            $this->design = $design;
+                        }
+
+
+
+                    }
+    
+                      // list of products
+                    $products =
+                    [
+                        new product('Ocean Breeze', 'blue', 'striped',),
+                        new product('Navy Drift', 'light blue', 'striped'),
+                        new product('Sunny Socks', 'yellow', 'solid'),
+                        new product('Glam Walkers', 'pink', 'striped'),
+                        new product('Crimson Web', 'red', 'striped'),
+                        new product('Cool Blue', 'blue', 'solid'),
+                        new product('Navy Drift', 'blue', 'solid'),
+                        new product('Cotton Candy', 'pink', 'solid'),
+                        new product('Tomato', 'red', 'solid'),
+                        new product('Lemon Whirl', 'yellow', 'striped'),
+                        new product('Sporty', 'white', 'striped'),
+                        new product('Long Stripes', 'multicolor', 'striped'),
+                        new product('White Dream', 'white', 'solid'),
+                        new product('Grey Dream', 'grey', 'solid'),
+                        new product('Confusion', 'multicolor', 'unique'),
+                        new product('Wooble Double', 'multicolor', 'unique'),
+                        new product('Splashy Colors', 'multicolor', 'unique'),
+                        new product('Warm Flowers', 'color', 'unique'),
+                    ];
+                
+                
 
                 // Search query checker
-                if(isset($_POST['search']) && !empty($_POST['search']))
+                if(isset($_SESSION["search"]))
                 {
-                    $searchQuery = strtolower($_POST['search']); // case sense
+                    $searchQuery = strtolower($_SESSION["search"]); // case sense
                     $filteredProducts = []; 
                     
-
                     // product loop
                     foreach($products as $product)
                     
@@ -58,17 +125,17 @@
                         $colorMatch = false;
 
                         //product check
-                        if (isset($product['name'])) 
+                        if (isset($product->name)) 
                         {
-                            $nameMatch = strpos(strtolower($product['name']), $searchQuery) !== false;
+                            $nameMatch = strpos(strtolower($product->name), $searchQuery) !== false;
                         }
-                        if (isset($product['design'])) 
+                        if (isset($product->design)) 
                         {
-                            $designMatch = strpos(strtolower($product['design']), $searchQuery) !== false;
+                            $designMatch = strpos(strtolower($product->design), $searchQuery) !== false;
                         }
-                        if (isset($product['color'])) 
+                        if (isset($product->color)) 
                         {
-                            $colorMatch = strpos(strtolower($product['color']), $searchQuery) !== false;
+                            $colorMatch = strpos(strtolower($product->color), $searchQuery) !== false;
                         }
 
                         //if there's a match, it enters the filtered products variable
@@ -78,14 +145,32 @@
                         }
                     }
                     //product display (the echos are temporary, I will update them with the actual products when the time comes)
-                    if (!empty($filteredProducts)) {
+                    if (!empty($filteredProducts)) 
+                    {
                         foreach ($filteredProducts as $product) 
                         {
-                            echo "<div class='product-item'>";
-                            echo "<h3>" . (isset($product['name']) ? $product['name'] : 'No name') . "</h3>"; 
-                            echo "<p>Color: " . (isset($product['color']) ? $product['color'] : 'No color') . "</p>";
-                            echo "<p>Design: " . (isset($product['design']) ? $product['design'] : 'No design') . "</p>"; 
+                            echo "<div id = 'products'>";
+
+                                if($product->name == 'Ocean Breeze')
+                                {
+                                    echo "<div class='ocean-breeze'>
+                                            <a href=''>
+                                                <img src='resources/sunny_socks_photos/catalogus/Sunny_socks_blue.jpg' alt='Ocean Breeze'>
+                                                <p>Ocean Breeze</p>
+                                                <p>$25.99</p>
+                                            </a>
+                                        </div>";
+                                }
+
+                                
+
                             echo "</div>";
+                            // echo "<div class='product-item'>";
+                            // echo "<h3>" . (isset($product->name) ? $product->name : 'No name') . "</h3>"; 
+                            // echo "<p>Color: " . (isset($product->color) ? $product->color : 'No color') . "</p>";
+                            // echo "<p>Design: " . (isset($product->design) ? $product->design : 'No design') . "</p>"; 
+                            // echo "</div>";
+
                         }
                     }
                     else 
@@ -95,11 +180,15 @@
                 }
                 else 
                 {
-                    
+                    echo "<p> test text</p>";
                 }
                 
                 ?>
             </div>
-        </div>
+    </main>
+</div>
+
+            
+        
     </body>
 </html>
