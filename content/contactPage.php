14f3a5df8a9d104251d5b5d06a443b15d41bd18c
Marcell-Bujtas <marcell.bujtas@student.nhlstenden.com>
2024-10-18 10:42:19 +0200

    Added contact title, set up the background image for it.

--- content/contactPage.php
+++ content/contactPage.php
@@ -1,1 +1,11 @@
-<h1>Contact</h1>
+<div id="contact-container">
+    <div id="contactPage-header">
+        <img src="./resources/otherPhotos/contact-image.png" alt="Photo of a wired phone.">
+        <div id="contact-title">
+            <h2>Contact</h2>
+        </div>
+    </div>
+    <div id="mid-container">
+
+    </div>
+</div>
