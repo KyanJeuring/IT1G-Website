3dfcb967c7f2c5398f9738f253acd1d12734ab15
KyanJeuring <kyanjeuring@gmail.com>
2024-10-19 11:18:10 +0200

    Changed the shopButton class into ctaButton
    This removes the unnecessary duplicate css

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
             <h1>Special Offer!</h1>
             <h2>Use Coupon Code</h2>
             <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST">
-                <button class="shopButton" type="submit" name="navBtn" value="Shop">FUNSOX</button>
+                <button class="ctaButton" type="submit" name="navBtn" value="Shop">FUNSOX</button>
             </form>
             <h2>for 30% off</h2>
         </div>
