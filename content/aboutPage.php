1d46d047018df0838cdd09070411d60f736dcc2a
Victoria Iascevschi <victoria.iascevschi@student.nhlstenden.com>
2024-10-20 18:04:46 +0200

    I started doing the section with reviews

--- content/aboutPage.php
+++ content/aboutPage.php
@@ -6,49 +6,96 @@
         <h1>WE CARE ABOUT YOU</h1>
         <h2>LET'S SAVE THE EARTH TOGETHER</h2>
         <p>
-         At Sunny, we believe that even the simplest daily actions, 
-         like putting on your socks, hold the potential to uplift spirits 
-         and add a splash of joy to life.
+            At Sunny, we believe that even the simplest daily actions, 
+            like putting on your socks, hold the potential to uplift spirits 
+            and add a splash of joy to life.
         </p>
         <p>
-         Our name, Sunny, reflects our core ethos: to always look on the bright side.
-         Our products do more than just clothe your feet—they embody positivity, 
-         add color to your routine, and create benefits for everyone involved, 
-         from the ground up.
+            Our name, Sunny, reflects our core ethos: to always look on the bright side.
+            Our products do more than just clothe your feet—they embody positivity, 
+            add color to your routine, and create benefits for everyone involved, 
+            rom the ground up.
         </p>
     </div>
 </section>
+
+    <section class="reviewsOnAboutPage">
+        <div class="slider-wrapper">
+            <div class="slider">
+                <div class="blueContainer">
+                    <div class="boxForReviews">
+                        <img class="photoCustomerMan" src="resources/otherPhotos/manPhoto.png" alt="photoCustomerMan">
+                        <div class="sectionOneforReviews">
+                            <div class="quotationMarks">
+                                <img id="slide-1" src="resources/otherPhotos/quotationMark.png" alt="quotationMarks">
+                            </div>
+                            <div class="nameReview">
+                                <h2>Random Person - student</h2>
+                                <img class="fourStarsReview" src="resources/otherPhotos/star_rating.png" alt="fourStarsReview">
+                            </div>
+                            <div class="descriptionToTheReviews">
+                            <h3>THE BEST PAIR OF SOCKS I OWN</h3>
+                                <p>
+                                    The fit and feel of these socks are fantastic. They’re soft, breathable, and keep my feet
+                                    fresh all day. The only downside is I wish there were more colour options, but 
+                                    the sustainability aspect more than makes up for it!
+                                </p>
+                            </div>
+                            <div class="upsideDownQuotes">
+                                <img class="upsideDownQuotesPhoto" src="resources/otherPhotos/upsideDownQuotes.png" alt="upsideDownQuotesPhoto">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>   
+            <div class="slider-nav">
+                <a href="#slide-1"></a>
+            </div>
+        </div>
+    </section>
+
 <section class="backgroundImageContainer">
     <h1>OUR VALUES</h1>
     <div class="contentOfThePage">
         <div class="boxPositivity">
-            <div class="headingPositivity"><h2>POSITIVITY</h2></div>
+            <div class="headingPositivity">
+                <h2>POSITIVITY</h2>
+            </div>
             <div class="positivityDescription">
-                Positivity is ingrained in our DNA. 
-                Sunny is all about embracing the bright side of life. We see every challenge as
-                an opportunity to innovate and deliver solutions that not only meet needs 
-                but exceed expectations.
+                <p>
+                    Positivity is ingrained in our DNA. 
+                    Sunny is all about embracing the bright side of life. We see every challenge as
+                    an opportunity to innovate and deliver solutions that not only meet needs 
+                    but exceed expectations.
+                </p>
             </div>
         </div>
         <div class="boxColorful">
-            <div class="headingColorful"><h3>COLORFUL</h3></div>
-                 <div class="colorfulDescription">
-                 We bring vibrancy to the everyday with socks that add a pop of color to your wardrobe. 
-                 By transforming the mundane—basic white socks—into a canvas of creativity,
-                 we celebrate individuality and joy.
-                 </div>
-        </div>
-        <div class="boxBeneficial">       
-            <div class="headingBeneficial"><h4>BENEFICIAL</h4></div>
-                <div class="beneficialDescription">
-                    At Sunny, we strive for win-win scenarios. Our business model ensures that everyone involved—from the materials 
-                    suppliers to our factory workers—benefits. Our products are crafted with respect for the Earth, 
-                    offering fair wages in our factories, fair profits for our retailers, and high-quality, 
-                    reasonably priced socks for our customers.
+           <div class="headingColorful">
+                <h3>COLORFUL</h3>
+           </div>
+               <div class="colorfulDescription">
+                    <p>
+                        We bring vibrancy to the everyday with socks that add a pop of color to your wardrobe. 
+                        By transforming the mundane—basic white socks—into a canvas of creativity,
+                        we celebrate individuality and joy.
+                    </p>
                 </div>
-            </div>
         </div>
-     <div class="filler"> 
-     </div>
-   
+                <div class="boxBeneficial">       
+                    <div class="headingBeneficial">
+                        <h4>BENEFICIAL</h4>
+                    </div>
+                    <div class="beneficialDescription">
+                        <p>
+                            At Sunny, we strive for win-win scenarios. Our business model ensures that everyone involved—from the materials 
+                            suppliers to our factory workers—benefits. Our products are crafted with respect for the Earth, 
+                            offering fair wages in our factories, fair profits for our retailers, and high-quality, 
+                            priced socks for our customers.
+                        </p>
+                    </div>
+                </div>
+    </div>
+        <div class="filler"> 
+        </div>
 </section>
