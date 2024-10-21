c33b387c81f8b988319afb8b63e4f2f11bf35447
Peter Kapsiar <kapsiar888n@gmail.com>
2024-10-21 13:16:29 +0200

    added donation functionality and further validation

--- storeLogic.php
+++ storeLogic.php
@@ -1,7 +1,6 @@
 <?php
     // PREPARE SESSION DATA
     if (!isset($_SESSION['shoppingCart']) || empty($_SESSION['shoppingCart'])) {
-        echo "New cart created";
         $_SESSION['shoppingCart'] = new shoppingCart();
     }  
     
@@ -13,17 +12,29 @@
     $address = filter_input(INPUT_POST, "address");
     $houseNumber = filter_input(INPUT_POST, "house-number");
     $city = filter_input(INPUT_POST, "city");
-    $state = filter_input(INPUT_POST, "state");
     $zip = filter_input(INPUT_POST, "zip");
     $country = filter_input(INPUT_POST, "country");
     $paymentMethod = filter_input(INPUT_POST, "payment-method", FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
     
+    $donation = filter_input(INPUT_POST, "donation", FILTER_VALIDATE_FLOAT);
+    $donationRst = filter_input(INPUT_POST, "donationRst", FILTER_VALIDATE_BOOLEAN);
     $coupon = filter_input(INPUT_POST, "coupon");
+    $couponRst = filter_input(INPUT_POST, "couponRst", FILTER_VALIDATE_BOOLEAN);
 
     // MAIN
-    if($coupon == "reset") $_SESSION['shoppingCart']->currentCoupon = null;
+    // coupon handling
+    if($couponRst) $_SESSION['shoppingCart']->currentCoupon = null;
+    //TODO: if coupon is false add error message
     else if(!empty($coupon)) $_SESSION['shoppingCart']->applyCoupon($coupon);
 
+    //donation handling
+    if($donationRst) $_SESSION['shoppingCart']->donation = null;
+    //TODO: if donation is false add error message
+    else if(isset($donation) && $donation != false) $_SESSION['shoppingCart']->donation = round($donation, 2);
+
+    //shipping cost handling
+    $_SESSION['shoppingCart']->shippingCountry = $country;
+    
     $_SESSION['shoppingCart']->calculatePrices();
 
     //TODO
