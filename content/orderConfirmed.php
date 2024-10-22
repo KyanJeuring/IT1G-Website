8f93ac46241f0feb6408120e4940d632bc86890c
Peter Kapsiar <kapsiar888n@gmail.com>
2024-10-23 00:48:58 +0200

    added styling to order confirmation page

--- content/orderConfirmed.php
+++ content/orderConfirmed.php
@@ -1,8 +1,11 @@
 <main>
-    <img src="" alt="checkmarkIcon">
-    <h1>Thank you for your order</h1>
-    <p>Your order has been submitted and will be shipped to you shortly.</p>
-    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
-        <button type="submit" name="navBtn" value="Home">Return to homepage</button>
-    </form>
+    <div id="orderSummary">
+        <img class="checkmark" src="resources/icons/svg/checkmark.svg" alt="checkmarkIcon">
+        <h1>Thank you for your order</h1>
+        <p>Your order has been submitted and will be shipped to you shortly.</p>
+        <!-- ADD ORDER SUMMARY -->
+        <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+            <button class="returnButton" type="submit" name="navBtn" value="Home">Return to homepage</button>
+        </form>
+    </div>
 </main>
