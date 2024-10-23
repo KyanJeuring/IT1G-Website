88f7fd1d4bb87f5f663a45678fdb37c02d89440d
Alexandros Karayiannis <alex-kar@hotmail.com>
2024-10-23 18:50:55 +0200

    commented out old code to experiment with new chat functionality. I made it be able to scroll on the y axis, styled it well

--- content/chatBox.php
+++ content/chatBox.php
@@ -1,5 +1,29 @@
-<div id="chatBox">
+<!-- <div id="chatBox">
     <button id="chatBtn">
         <img src="resources/icons/svg/chat-bubble.svg" alt="chatBubble">
     </button>
+</div> -->
+
+
+
+<div class="chatBot">
+    <header>
+        <h2>Chatbot</h2>
+    </header>
+    
+    <ul class="chatBox">
+        <li class="chat incoming">
+            <img src="./resources/icons/chatBotIcon.png" alt="chat bot icon">
+            <p>Hi there 👋 <br> How can I help you today?</p>
+        </li>
+
+        <li class="chat outgoing">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </li>
+    </ul>
+
+    <form class="chatInput">
+        <textarea placeholder="I would love some help with..." required></textarea>
+        <button type="submit"><img src="./resources/icons/sendButtonChatbox.png" alt="send button"></button>
+    </form>
 </div>
